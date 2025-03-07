b3d495baeac0e663a17d0eedd1643f1f17aa429b
nathkeuss <julio.nathan47@gmail.com>
2025-03-07 18:11:00 +0100

    feat(continent): add list of continents with associated countries

---
+++ src/Controller/Public/ContinentController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Public;
+
+use App\Repository\ContinentRepository;
+use App\Repository\CountryRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContinentController extends AbstractController
+{
+    #[Route('/continent/{slugContinent}', name: 'continent_show', methods: ['GET'], requirements: ['slugContinent' => '[a-zA-Z0-9-]+'])]
+    public function showContinent(string              $slugContinent,
+                                  ContinentRepository $continentRepository,
+                                  CountryRepository   $countryRepository)
+    {
+        $continent = $continentRepository->findOneBy(['slug' => $slugContinent]);
+        $countries = $countryRepository->findBy(['continent' => $continent]);
+
+        return $this->render('public/continent/show.html.twig', [
+            'continent' => $continent,
+            'countries' => $countries
+        ]);
+
+    }
+
+
+}
--- src/Controller/Public/CountryController.php
+++ src/Controller/Public/CountryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Public;
 
+use App\Repository\ContinentRepository;
 use App\Repository\CountryRepository;
 use App\Repository\TutorialRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,16 +11,19 @@ use Symfony\Component\Routing\Annotation\Route;
 class CountryController extends AbstractController
 {
     #[Route('/country/list', name: 'country_list', methods: ['GET'])]
-    public function listCountries(CountryRepository $countryRepository)
+    public function listCountriesAndContinent(CountryRepository   $countryRepository,
+                                              ContinentRepository $continentRepository)
     {
         $countries = $countryRepository->findAll();
+        $continents = $continentRepository->findAll();
 
         return $this->render('public/country/list.html.twig', [
-            'countries' => $countries
+            'countries' => $countries,
+            'continents' => $continents
         ]);
     }
 
-    #[Route('/country/{slugCountry}', name: 'country_show', methods: ['GET'])]
+    #[Route('/continent/{slugContinent}/country/{slugCountry}', name: 'country_show', methods: ['GET'])]
     public function showCountry(string             $slugCountry,
                                 CountryRepository  $countryRepository,
                                 TutorialRepository $tutorialRepository)
