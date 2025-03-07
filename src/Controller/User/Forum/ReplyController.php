2fa2e548fe70b2068893d93af4cc56dc86ed2ce8
nathkeuss <julio.nathan47@gmail.com>
2025-03-07 20:29:12 +0100

    feat(reply): add update and delete functionality

--- src/Controller/User/Forum/ReplyController.php
+++ src/Controller/User/Forum/ReplyController.php
@@ -4,6 +4,7 @@ namespace App\Controller\User\Forum;
 
 use App\Entity\Reply;
 use App\Form\ReplyType;
+use App\Repository\ReplyRepository;
 use App\Repository\TopicRepository;
 use App\Service\ImageUploader;
 use Doctrine\ORM\EntityManagerInterface;
@@ -56,7 +57,75 @@ class ReplyController extends AbstractController
             'formReplyView' => $formReplyView
         ]);
 
+    }
 
+    #[Route('/forum/topic/{id}/reply/update/{replyId}', name: 'reply_update', methods: ['GET', 'POST'])]
+    public function updateReply(int $id,
+                                int $replyId,
+                                Request $request,
+                                TopicRepository $topicRepository,
+                                ReplyRepository $replyRepository,
+                                EntityManagerInterface $entityManager,
+                                ImageUploader $imageUploader)
+    {
+        $topic = $topicRepository->find($id);
+        $reply = $replyRepository->find($replyId);
+
+        if ($reply->getUser() !== $this->getUser()) {
+            return $this->redirectToRoute('topic_show', ['id' => $id]);
+        }
+
+        $formReply = $this->createForm(ReplyType::class, $reply);
+
+        $formReply->handleRequest($request);
+
+        if ($formReply->isSubmitted() && $formReply->isValid()) {
+
+            $replyImage = $formReply->get('image')->getData();
+
+            if ($replyImage) {
+                $imageUploader->removeImage('forum/replies', $reply->getImage());
+                $imageNewFilename = $imageUploader->uploadImage($replyImage, 'forum/replies');
+                $reply->setImage($imageNewFilename);
+            }
+
+            $entityManager->flush();
+            $entityManager->persist($reply);
+
+            $this->addFlash('success', 'La réponse a bien été modifiée');
+            return $this->redirectToRoute('topic_show', ['id' => $id]);
+        }
+
+        $formReplyView = $formReply->createView();
+
+        return $this->render('user/forum/reply/update.html.twig', [
+            'formReplyView' => $formReplyView
+        ]);
+
+    }
+
+    #[Route('/forum/topic/{id}/reply/delete/{replyId}', name: 'reply_delete', methods: ['GET'])]
+    public function deleteReply(int $id,
+                                int $replyId,
+                                TopicRepository $topicRepository,
+                                ReplyRepository $replyRepository,
+                                EntityManagerInterface $entityManager,
+                                ImageUploader $imageUploader)
+    {
+        $topic = $topicRepository->find($id);
+        $reply = $replyRepository->find($replyId);
+
+        if ($reply->getUser() !== $this->getUser()) {
+            return $this->redirectToRoute('topic_show', ['id' => $id]);
+        }
+
+        $imageUploader->removeImage('forum/replies', $reply->getImage());
+
+        $entityManager->remove($reply);
+        $entityManager->flush();
+
+        $this->addFlash('success', 'La réponse a bien été supprimée');
+        return $this->redirectToRoute('topic_show', ['id' => $id]);
     }
 
 }
