587f5eb07351c4e1b32e01ec70637f316562b167
nathkeuss <julio.nathan47@gmail.com>
2025-03-07 19:33:01 +0100

    feat(topic): add update functionality

--- src/Controller/User/Forum/TopicController.php
+++ src/Controller/User/Forum/TopicController.php
@@ -14,9 +14,9 @@ use Symfony\Component\Routing\Annotation\Route;
 class TopicController extends AbstractController
 {
     #[Route('/forum/topic/create', name: 'topic_create', methods: ['GET', 'POST'])]
-    public function createTopic(Request                 $request,
-                                EntityManagerInterface  $entityManager,
-                                ImageUploader           $imageUploader)
+    public function createTopic(Request                $request,
+                                EntityManagerInterface $entityManager,
+                                ImageUploader          $imageUploader)
     {
         if (!$this->getUser()) {
             return $this->redirectToRoute('login_user');
@@ -54,7 +54,8 @@ class TopicController extends AbstractController
     }
 
     #[Route('/forum/topic/list', name: 'topic_list', methods: ['GET'])]
-    public function listTopics(TopicRepository $topicRepository) {
+    public function listTopics(TopicRepository $topicRepository)
+    {
 
         $topics = $topicRepository->findAll();
 
@@ -71,6 +72,49 @@ class TopicController extends AbstractController
 
         return $this->render('public/forum/topic/show.html.twig', [
             'topic' => $topic
+        ]);
+
+    }
+
+    #[Route('/forum/topic/update/{id}', name: 'topic_update', methods: ['GET', 'POST'])]
+    public function updateTopic(int                    $id,
+                                Request                $request,
+                                EntityManagerInterface $entityManager,
+                                TopicRepository        $topicRepository,
+                                ImageUploader          $imageUploader)
+    {
+        $topic = $topicRepository->find($id);
+
+        if ($topic->getUser() !== $this->getUser()) {
+            return $this->redirectToRoute('topic_list');
+        }
+
+        $formTopic = $this->createForm(TopicType::class, $topic);
+
+        $formTopic->handleRequest($request);
+
+        if ($formTopic->isSubmitted() && $formTopic->isValid()) {
+
+            $topicImage = $formTopic->get('image')->getData();
+
+            if ($topicImage) {
+                $imageUploader->removeImage('forum/topics', $topic->getImage());
+                $imageNewFilename = $imageUploader->uploadImage($topicImage, 'forum/topics');
+                $topic->setImage($imageNewFilename);
+            }
+
+            $entityManager->flush();
+            $entityManager->persist($topic);
+
+            $this->addFlash('success', 'Le topic a bien été modifié');
+            return $this->redirectToRoute('topic_show', ['id' => $id]);
+
+        }
+
+        $formTopicView = $formTopic->createView();
+
+        return $this->render('user/forum/topic/update.html.twig', [
+            'formTopicView' => $formTopicView
         ]);
 
     }
