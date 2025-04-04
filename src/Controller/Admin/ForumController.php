81388479eba2dc846e97fb6887ddb6e3121cd5f2
nathkeuss <julio.nathan47@gmail.com>
2025-04-04 19:12:20 +0200

    feat(forum): add topic and reply deletion from admin panel

--- src/Controller/Admin/ForumController.php
+++ src/Controller/Admin/ForumController.php
@@ -4,6 +4,8 @@ namespace App\Controller\Admin;
 
 use App\Repository\ReplyRepository;
 use App\Repository\TopicRepository;
+use App\Service\ImageUploader;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -41,6 +43,52 @@ class ForumController extends AbstractController
             'topic' => $topic,
             'replies' => $replies
         ]);
+    }
+
+    #[Route('/admin/forum/topic/delete/{id}', name: 'admin_topic_delete', methods: ['GET'])]
+    public function deleteTopic(int             $id,
+                                EntityManagerInterface $entityManager,
+                                ImageUploader $imageUploader,
+                                TopicRepository $topicRepository,
+                                ReplyRepository $replyRepository): Response
+    {
+        $topic = $topicRepository->find($id);
+
+        $replies = $replyRepository->findBy(['topic' => $topic]);
+        foreach ($replies as $reply) {
+            $imageUploader->removeImage('forum/replies', $reply->getImage());
+            $entityManager->remove($reply);
+        }
+
+        $imageUploader->removeImage('forum/topics', $topic->getImage());
+
+        $entityManager->remove($topic);
+        $entityManager->flush();
+
+        $this->addFlash('success', 'Le topic a bien été supprimé');
+        return $this->redirectToRoute('admin_topic_list');
+
+    }
+
+    #[Route('/admin/forum/topic/{id}/reply/delete/{replyId}', name: 'admin_reply_delete', methods: ['GET'])]
+    public function deleteReply(int $id,
+                                int $replyId,
+                                TopicRepository $topicRepository,
+                                ReplyRepository $replyRepository,
+                                EntityManagerInterface $entityManager,
+                                ImageUploader $imageUploader)
+    {
+        $topic = $topicRepository->find($id);
+        $reply = $replyRepository->find($replyId);
+
+
+        $imageUploader->removeImage('forum/replies', $reply->getImage());
+
+        $entityManager->remove($reply);
+        $entityManager->flush();
+
+        $this->addFlash('success', 'La réponse a bien été supprimée');
+        return $this->redirectToRoute('admin_topic_show', ['id' => $id]);
     }
 
 }
