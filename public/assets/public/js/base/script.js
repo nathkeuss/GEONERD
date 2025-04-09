daa16ba36982e6ef46e994e9dd4b0a51106873e3
nathkeuss <julio.nathan47@gmail.com>
2025-04-09 02:40:19 +0200

    feat(responsive): improve responsive

--- public/assets/public/js/base/script.js
+++ public/assets/public/js/base/script.js
@@ -1,12 +1,14 @@
 const profileMenu = document.getElementById('profile-menu');
 const profileDropdown = document.getElementById('profile-dropdown');
 
-profileMenu.addEventListener('click', function() {
+profileMenu.addEventListener('click', function () {
     profileDropdown.classList.toggle('show');
 });
 
-document.addEventListener('click', function(event) {
+document.addEventListener('click', function (event) {
     if (!profileMenu.contains(event.target)) {
         profileDropdown.classList.remove('show');
     }
 });
+
+
