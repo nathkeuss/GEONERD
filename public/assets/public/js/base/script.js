f70e20ece932d97ad63f40e086948d7655a0fc58
nathkeuss <julio.nathan47@gmail.com>
2025-04-09 19:16:08 +0200

    feat(improve responsive and add favicon): improve responsive and add favicon

--- public/assets/public/js/base/script.js
+++ public/assets/public/js/base/script.js
@@ -1,14 +1,24 @@
 const profileMenu = document.getElementById('profile-menu');
 const profileDropdown = document.getElementById('profile-dropdown');
 
-profileMenu.addEventListener('click', function () {
-    profileDropdown.classList.toggle('show');
-});
+if (profileMenu && profileDropdown) {
+    profileMenu.addEventListener('click', function () {
+        profileDropdown.classList.toggle('show');
+    });
 
-document.addEventListener('click', function (event) {
-    if (!profileMenu.contains(event.target)) {
-        profileDropdown.classList.remove('show');
-    }
+    document.addEventListener('click', function (event) {
+        if (!profileMenu.contains(event.target)) {
+            profileDropdown.classList.remove('show');
+        }
+    });
+}
+
+
+const mobileBurger = document.getElementById('mobile-burger');
+const mobileMenu = document.getElementById('mobile-menu');
+
+mobileBurger.addEventListener('click', () => {
+    mobileMenu.classList.toggle('open');
 });
 
 
